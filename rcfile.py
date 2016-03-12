30044df119a717a2514fe3ab1e85c8d80b6d7e79
fan <wyr123321@gmail.com>
2016-03-12 13:04:52 +0800

    a file

--- rcfile.py
+++ rcfile.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
-from random import choice
+from random import choice,randint
 a = '''qwertyuiopasdfghjkl;zxcvbnm,./\'
 []\\1234567890-=`~!@#$%^&*()_+
 {}|:"<>?QWERTYUIOPASDFGHJKLZXCVBNM'''
-files = open('tmp.txt','w')
-for i in range(2500):
+temp = []
+for i in range(randint(1,10)):
+    temp.append(a[randint(0,len(a)-1)])
+files = open(''.join(temp)+'.txt','w')
+for i in range(randint(1,2**16)):
     files.write(choice(a))
 files.close()
