f716509ce8e66aae91d4eeea42d068e8f1fdbf4e
fan <wyr123321@gmail.com>
2016-04-01 20:44:25 +0800

    update rcfile.py

--- rcfile.py
+++ rcfile.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python3
+import argparse
 from random import choice,randint
-a = '''qwertyuiopasdfghjkl;zxcvbnm,./\'
-[]\\1234567890-=`~!@#$%^&*()_+
-{}|:"<>?QWERTYUIOPASDFGHJKLZXCVBNM'''
-temp = []
-for i in range(randint(1,10)):
-    temp.append(a[randint(0,len(a)-1)])
-files = open(''.join(temp)+'.txt','w')
-for i in range(randint(1,2**16)):
-    files.write(choice(a))
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file')
+parser.add_argument('-s','--size',type=int,default=-1)
+args = parser.parse_args()
+myfile = args.file
+char_size = args.size
+
+a = 'qwertyuiopasdfghjkl;zxcvbnm,./\'[]\\1234567890-=`~!@#$%^&*()_+{}|:"<>?QWERTYUIOPASDFGHJKLZXCVBNM'
+if myfile:
+    temp = myfile
+else:
+    temp = []
+    for i in range(randint(1,10)):
+        temp.append(a[randint(0,len(a)-1)])
+    temp = ''.join(temp) + '.txt'
+files = open(temp,'w')
+if char_size == -1:
+    num = randint(1,2**16)
+else:
+    num = char_size
+for i in range(num):
+    files.write(choice(a))    
 files.close()
